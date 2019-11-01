748109b416d1b953a8f0d0d6b9733b926f56fc24
PELANT Antonín <antonin.pelant@cns.cz>
2019-11-01 09:40:55 +0100

    added solution and projects

---
+++ StatsFrom/StatsFrom.Data/StatsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatsFrom.Data
+{
+    public class StatsDbContext : DbContext
+    {
+
+    }
+}
